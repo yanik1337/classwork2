bc765379fcad9392d54c27826ca084dc9934aeb0
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-28 18:15:29 +0300

    iorderlogisticsinterface
    eqweqedqweqweqweqweqweqweqweqweqeqweqw

---
+++ IOrderLogisticsInterface.cs
@@ -0,0 +1,11 @@
+﻿using ClassWork4.OrderClass;
+
+namespace ClassWork4.IOrderLogisticsInterface
+{
+    public interface IOrderLogistics
+    {
+        void ProcessOrder(Order order);
+        void ChangeOrderStatus(int orderId, string newStatus);
+        void UpdateProductQuantity(int productId, int quantityChange);
+    }
+}
