3612f70f5db37b915613bd965f0fc002d5e30c6e
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-28 18:13:46 +0300

    productclass
    сделано не до конца(не все задания)

---
+++ ProductClass.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassWork4.ProductClass
+{
+    public class Product
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+        public int Quantity { get; set; }
+
+        public Product(int id, string name, decimal price, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
